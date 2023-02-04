2d2179dc6d75c817405a9bdfaa82a995055d69fb
Stefan Partheymüller <s.partheymueller@initics.de>
2023-02-04 22:55:14 +0100

    add noconfirm-option in uninstall.sh for all package manager commands

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,9 @@
 
 # Configure package manager here if necessary:
 if [ -f /bin/yay ]; then
-  PKGMAN=yay
+  PKGMAN="yay -Rsn --noconfirm"
 elif [ -f /bin/paru ]; then
-  PKGMAN=paru
+  PKGMAN="paru -Rsn --noconfirm"
 else
   echo "ERROR: Couldn't find a package manager, please configure it manually"
   exit 1
@@ -17,14 +17,14 @@ PKGSUFFIX=archfix
 sudo systemctl stop v4l2-relayd.service
 sudo systemctl disable v4l2-relayd.service
 
-$PKGMAN -Rsn intel-ivsc-driver-dkms-git
-$PKGMAN -Rsn intel-ivsc-firmware
-$PKGMAN -Rsn icamerasrc-git
-$PKGMAN -Rsn intel-ipu6ep-camera-hal-git-${PKGSUFFIX}
-$PKGMAN -Rsn intel-ipu6ep-camera-bin
-$PKGMAN -Rsn intel-ipu6-dkms-git-${PKGSUFFIX}
-$PKGMAN -Rsn v4l2-relayd-${PKGSUFFIX}
-$PKGMAN -Rsn v4l2loopback-dkms-git-${PKGSUFFIX}
+$PKGMAN intel-ivsc-driver-dkms-git
+$PKGMAN intel-ivsc-firmware
+$PKGMAN icamerasrc-git
+$PKGMAN intel-ipu6ep-camera-hal-git-${PKGSUFFIX}
+$PKGMAN intel-ipu6ep-camera-bin
+$PKGMAN intel-ipu6-dkms-git-${PKGSUFFIX}
+$PKGMAN v4l2-relayd-${PKGSUFFIX}
+$PKGMAN v4l2loopback-dkms-git-${PKGSUFFIX}
 
 if [ -d /etc/systemd/system/v4l2-relayd.service.d ]; then
   sudo rm -rf /etc/systemd/system/v4l2-relayd.service.d/

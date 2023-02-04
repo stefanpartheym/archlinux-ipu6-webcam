b8871d106c0177fea85c8a16cbb17b17fbe47a59
Stefan Partheymüller <s.partheymueller@initics.de>
2023-02-04 21:55:54 +0100

    rename patched packages to avoid conflicts with their AUR counter parts

--- uninstall.sh
+++ uninstall.sh
@@ -10,17 +10,21 @@ else
   exit 1
 fi
 
+# The package suffix used to install the patched packages to not conflict with
+# their AUR counter part:
+PKGSUFFIX=archfix
+
 sudo systemctl stop v4l2-relayd.service
 sudo systemctl disable v4l2-relayd.service
 
 $PKGMAN -Rsn intel-ivsc-driver-dkms-git
 $PKGMAN -Rsn intel-ivsc-firmware
 $PKGMAN -Rsn icamerasrc-git
-$PKGMAN -Rsn intel-ipu6ep-camera-hal-git
+$PKGMAN -Rsn intel-ipu6ep-camera-hal-git-${PKGSUFFIX}
 $PKGMAN -Rsn intel-ipu6ep-camera-bin
-$PKGMAN -Rsn intel-ipu6-dkms-git
-$PKGMAN -Rsn v4l2-relayd
-$PKGMAN -Rsn v4l2loopback-dkms-git
+$PKGMAN -Rsn intel-ipu6-dkms-git-${PKGSUFFIX}
+$PKGMAN -Rsn v4l2-relayd-${PKGSUFFIX}
+$PKGMAN -Rsn v4l2loopback-dkms-git-${PKGSUFFIX}
 
 if [ -d /etc/systemd/system/v4l2-relayd.service.d ]; then
   sudo rm -rf /etc/systemd/system/v4l2-relayd.service.d/

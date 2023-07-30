db7268acb325bceb08de56ec13d0776bad490619
Stefan Partheymüller <s.partheymueller@initics.de>
2023-07-31 00:00:32 +0200

    add .editorconfig to enforce a universal coding style for shell scripts (the main reason being that i switched to nvim recently with shfmt preconfigured, which - without the .editorconfig - would fuck up the formatting in the shell scripts)

--- test.sh
+++ test.sh
@@ -3,9 +3,11 @@
 # Configure sink depending on running window manager
 case "${XDG_SESSION_TYPE}" in
   wayland)
-    SINK=waylandsink ;;
+    SINK=waylandsink
+    ;;
   x11)
-    SINK=ximagesink ;;
+    SINK=ximagesink
+    ;;
 esac
 
 sudo -E LANG=C gst-launch-1.0 icamerasrc ! video/x-raw,format=NV12,width=1280,height=720 ! videoconvert ! "${SINK}"

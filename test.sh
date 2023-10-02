159b2938dfd8906d740131e4f8149a7f5c44f732
Alvin Alvarado <alvin@striczkof.io>
2023-10-02 22:38:25 +1100

    Change format to YUY2 if using Tiger Lake

--- test.sh
+++ test.sh
@@ -10,4 +10,14 @@ case "${XDG_SESSION_TYPE}" in
     ;;
 esac
 
-sudo -E LANG=C gst-launch-1.0 icamerasrc ! video/x-raw,format=NV12,width=1280,height=720 ! videoconvert ! "${SINK}"
+# Check if on Tiger Lake by checking if 'intel-ipu6-camera-hal-git[-fix]' is installed.
+if pacman -Qq intel-ipu6-camera-hal-git-fix >/dev/null 2>&1 \
+ || pacman -Qq intel-ipu6-camera-hal-git >/dev/null 2>&1; then
+  # Set to YUY2 for Tiger Lake
+  FORMAT=YUY2
+else
+  # Set to NV12 for Alder Lake and Meteor Lake
+  FORMAT=NV12
+fi
+
+sudo -E LANG=C gst-launch-1.0 icamerasrc ! video/x-raw,format="${FORMAT}",width=1280,height=720 ! videoconvert ! "${SINK}"
